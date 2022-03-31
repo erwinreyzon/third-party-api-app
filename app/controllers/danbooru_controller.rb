32e51540c2f74a8bd26e103084060a3b053346d6
erwin <winzon353535@gmail.com>
2022-03-31 14:32:18 -0700

    adds index to controller

--- app/controllers/danbooru_controller.rb
+++ app/controllers/danbooru_controller.rb
@@ -1,2 +1,7 @@
 class DanbooruController < ApplicationController
+
+  def index
+    response = HTTP.get("https://danbooru.donmai.us/profile.json?login=#{Rails.application.credentials.user_name}&api_key=#{Rails.application.credentials.danbooru.api_key}")
+    render json: response.parse(:json)
+  end
 end
